e33eca4ba01c48c1ef3b36882095acb724ef5f1d
Matthias Thubauville <matthias.thubo@gmail.com>
2015-03-01 06:14:52 -0500

    Inital commit with gitignore.d config

---
+++ .i3/xrandr.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+NUM_DISP=$(xrandr -q | grep -w connected | wc -l)
+
+if [[ $NUM_DISP == 1 ]]; then
+  xrandr \
+    --output LVDS1 --auto
+else
+
+  if [[ $HOSTNAME == "X230" ]]; then
+    xrandr \
+      --output LVDS1 --off \
+      --output HDMI3 --auto --rotate normal --primary \
+      --output HDMI2 --auto --rotate left --right-of HDMI3
+  elif [[ $HOSTNAME == "eyrie" ]]; then
+    xrandr \
+      --output eDP1 --auto --primary \
+      --output HDMI1 --auto --right-of eDP1
+  fi
+
+fi
