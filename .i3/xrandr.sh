07e7c0aa2929eb035d3b7b38801cf4c65131771c
Matthias Thubauville <matthias.thubo@gmail.com>
2015-07-01 21:52:41 +0200

    Updates for x230 config

--- .i3/xrandr.sh
+++ .i3/xrandr.sh
@@ -5,16 +5,8 @@ if [[ $NUM_DISP == 1 ]]; then
   xrandr \
     --output LVDS1 --auto
 else
-
-  if [[ $HOSTNAME == "X230" ]]; then
-    xrandr \
-      --output LVDS1 --off \
-      --output HDMI2 --auto --rotate normal --primary \
-      --output HDMI3 --auto --rotate left --right-of HDMI2
-  elif [[ $HOSTNAME == "eyrie" ]]; then
-    xrandr \
-      --output eDP1 --auto --primary \
-      --output HDMI1 --auto --right-of eDP1
-  fi
-
+  xrandr \
+    --output LVDS1 --off \
+    --output HDMI2 --auto --rotate normal --primary \
+    --output HDMI3 --auto --rotate left --right-of HDMI2
 fi
