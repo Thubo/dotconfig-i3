cda29cf4ac1353c3738a40d789416ded414e535b
Matthias Thubauville <matthias.thubo@gmail.com>
2015-07-08 11:08:48 +0200

    Updated xrandr script for eyrie

--- .i3/xrandr.sh
+++ .i3/xrandr.sh
@@ -6,15 +6,8 @@ if [[ $NUM_DISP == 1 ]]; then
     --output LVDS1 --auto
 else
 
-  if [[ $HOSTNAME == "X230" ]]; then
-    xrandr \
-      --output LVDS1 --off \
-      --output HDMI3 --auto --rotate normal --primary \
-      --output HDMI2 --auto --rotate left --right-of HDMI3
-  elif [[ $HOSTNAME == "eyrie" ]]; then
     xrandr \
       --output eDP1 --auto --primary \
       --output HDMI1 --auto --right-of eDP1
-  fi
 
 fi
